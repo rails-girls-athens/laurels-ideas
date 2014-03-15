cec3b8e92f8fcd9a234322c51ad396b5908968a2
Jordan Burke <me@jordankburke.com>
2014-03-15 14:50:25 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Laurel::Application.config.secret_key_base = 'f061ef3d9c5483aa7177c048dc2f347844b8324bce4e3e468cd5e0997325fe59bb0c77c8fcc15074c1ee79ce4564749014a47dcb39b80deb10d2f458cd08784d'
